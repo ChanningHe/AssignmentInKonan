05b2d3192e1d1f7fe8aca5fa16b312a839acf516
ChanningHe <45251102@qq.com>
2022-01-22 07:17:24 +0000

    KaShouen

---
+++ AssignmentInKonan/freshman/2/15/week15.c
@@ -0,0 +1,34 @@
+/* 
+12171029 KaShouen
+https://github.com/ChanningHe/AssignmentInKonan
+ */
+
+#include<stdio.h>
+#define LEN (100)
+
+/* 文字列を圧縮する関数．これを自分で書くこと．この関数内にprintfなど
+   の出力を書かないこと */
+
+void compress(適切な引数){
+
+  
+}
+
+
+/* メイン関数．書き換えないこと */
+
+int main(){
+
+  char dna[LEN];
+
+  /* 文字列を入力 */
+  scanf("%99s", dna);
+
+  /* 関数呼び出し */
+  compress(dna);
+
+  /* 結果表示 */
+  printf("%s\n", dna);
+
+  return 0;
+}
